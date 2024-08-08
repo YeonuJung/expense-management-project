6e58d5b5c2cf7e57d007f5c1e6f932b9533cbff6
YeonuJung <gsh95214@naver.com>
2024-08-08 17:09:08 +0900

    추가: 마커에 마우스 호버했을 때 infoWindow 보이도록 추가, 로드에러 및 로드 안되었을 때 로딩 추가, 수정: 지도 색상커스텀 및 마커 색상 변경, Loadscript삭제 및 useJsApiLoader로 변경

--- src/components/Pages/VisitedPlace/VisitedPlace.tsx
+++ src/components/Pages/VisitedPlace/VisitedPlace.tsx
@@ -1,14 +1,17 @@
 import {
   GoogleMap,
-  LoadScript,
+  useJsApiLoader,
   Marker,
   InfoWindow,
+  Libraries,
 } from "@react-google-maps/api";
 import Appbar from "../../Organism/Appbar/Appbar";
 import Sidebar from "../../Organism/Sidebar/Sidebar";
 import "./VisitedPlace.scss";
 import { useEffect, useState } from "react";
 import { setDefaults, fromAddress, OutputFormat } from "react-geocode";
+
+const libraries: Libraries = ["places"];
 
 setDefaults({
   key: process.env.REACT_APP_GOOGLE_MAP_API_KEY as string,
@@ -27,15 +30,135 @@ const containerStyle = {
   width: "100%",
   height: "800px",
 };
+const customMapStyles = [
+  {
+    elementType: "geometry",
+    stylers: [{ color: "#111827" }],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#ffffff" }],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#111827" }],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#5048e5" }],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#5048e5" }],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#10B981" }],
+  },
+  {
+    featureType: "landscape.natural",
+    elementType: "geometry",
+    stylers: [{ color: "#1f2937" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [{ color: "#1f2937" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#10B981" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry.fill",
+    stylers: [{ color: "#1f2937" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#10B981" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#374151" }],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [{ color: "#374151" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#374151" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#5048e5" }],
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry",
+    stylers: [{ color: "#374151" }],
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#5048e5" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#374151" }],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry.fill",
+    stylers: [{ color: "#1f2937" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#10B981" }],
+  },
+  {
+    featureType: "poi.business",
+    elementType: "geometry.fill",
+    stylers: [{ color: "#5048e5" }],
+  },
+  {
+    featureType: "poi.business",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#10B981" }],
+  },
+  {
+    featureType: "poi.business",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#111827" }],
+  },
+ 
+];
 function VisitedPlace() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY as string,
+    libraries: libraries,
+  });
   const [location, setLocation] = useState<Location[]>([]);
   const addresses: string[] = ["송리단길", "코엑스", "이태원"];
   const [map, setMap] = useState<google.maps.Map | null>(null);
-  const [center, setCenter] = useState<Omit<Location, "info">>({
+  const [center] = useState<Omit<Location, "info">>({
     lat: 37.5665,
     lng: 126.978,
   });
   const [selectedMarker, setSelectedMarker] = useState<Location | null>(null);
+  const [hoveredMarker, setHoveredMarker] = useState<Location | null>(null);
 
   useEffect(() => {
     const fetchCoordinate = async () => {
@@ -58,7 +181,7 @@ function VisitedPlace() {
   }, []);
 
   const handleMarkerClick = (location: Location) => {
-      setSelectedMarker({ ...location });
+      setSelectedMarker({...location});
       map?.setZoom(14);
       map?.panTo(location);
   };
@@ -66,7 +189,20 @@ function VisitedPlace() {
   const handleInfoWindowCloseClick = () => {
     setSelectedMarker(null);
   };
+  
+  const handleMarkerHover = (location: Location) => {
+    setHoveredMarker({...location});
+  }
+  const handleMarkerUnhover = () => {
+    setHoveredMarker(null);
+  }
+  if (loadError) {
+    return <div>Error loading Google Maps API</div>;
+  }
 
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="visitedPlace__container">
       <Sidebar />
@@ -81,17 +217,12 @@ function VisitedPlace() {
             <div className="visitedPlace__notice">
               ℹ️지출 내역 정보의 품질에 따라 데이터가 지도에 나타나지 않을 수도 있습니다. ℹ️
             </div>
-            <LoadScript
-              googleMapsApiKey={
-                process.env.REACT_APP_GOOGLE_MAP_API_KEY as string
-              }
-              loadingElement={<div>Loading..."로딩이 계속 발생하는 경우 새로고침 해주세요"</div>}
-            >
               <GoogleMap
                 mapContainerStyle={containerStyle}
                 center={center}
                 zoom={10}
                 onLoad={(map) => setMap(map)}
+                options={{ styles: customMapStyles}}
                 
               >
                 {location.map((loc, idx) => (
@@ -99,29 +230,31 @@ function VisitedPlace() {
                     key={idx}
                     position={loc}
                     onClick={() => handleMarkerClick(loc)}
+                    onMouseOver={() => handleMarkerHover(loc)}
+                    onMouseOut={handleMarkerUnhover}
                     icon={{
-                      url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png",
-                      scaledSize: new window.google.maps.Size(40, 40),
+                      url: `http://maps.google.com/mapfiles/ms/icons/yellow-dot.png`,
+                      scaledSize: new window.google.maps.Size(45, 45),
                     }}
                     animation={window.google.maps.Animation.DROP}
                     options={{cursor: 'url(data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA9AAAAfwAAAOqAAAHqgAAB6sAAA//AAAd/wAAGf8AAAG9AAABtAAAAYAAAAGAAAABgAAAAYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/////////////////////////////////////////////gn///4B///8Af//+AD///AA///wAH//4AB//8AAf//AAH//5AB///wA///8A////D////w////8P////n////////////////////////////////////////////8=), auto' }}
                   ></Marker>
                 ))}
-                {selectedMarker && (
+                {(selectedMarker || hoveredMarker) && (
                   <InfoWindow
                   position={{
-                    lat: selectedMarker.lat,
-                    lng: selectedMarker.lng,
+                    lat: (selectedMarker || hoveredMarker)?.lat as number,
+                    lng: (selectedMarker || hoveredMarker)?.lng as number,
                   }}
                     onCloseClick={handleInfoWindowCloseClick}
+                    options={{ pixelOffset: new window.google.maps.Size(0, -55) }}
                   >
                     {/* name부분을 같이 보여주는 걸로 하기 */}
                     {/* 밑에 selectedMarker.info는 데이터의 place로 바꾸기 */}
-                    <div className="infoWindow__content">{selectedMarker.info}</div>
+                    <div className="infoWindow__content">{(selectedMarker || hoveredMarker)?.info}</div>
                   </InfoWindow>
                 )}
               </GoogleMap>
-            </LoadScript>
           </div>
         </div>
       </div>
